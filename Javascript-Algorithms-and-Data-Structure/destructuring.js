e3d3c1ed3870785809d286df6037a764b7412d3f
Jingcheng Wang <jingwang49@gmail.com>
2023-11-09 22:58:43 +1100

    setup destructuring

---
+++ Javascript-Algorithms-and-Data-Structure/destructuring.js
@@ -0,0 +1,13 @@
+// Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const today = HIGH_TEMPERATURES.today;
+  const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
+  // Only change code above this line
