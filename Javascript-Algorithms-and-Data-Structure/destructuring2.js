2dc0a7702433bce83e7d0435c9942131cc5048b3
Jingcheng Wang <jingwang49@gmail.com>
2023-11-09 23:01:26 +1100

    setup desctructuring2

---
+++ Javascript-Algorithms-and-Data-Structure/destructuring2.js
@@ -0,0 +1,13 @@
+// Use Destructuring Assignment to Assign Variables from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+    
+  const highToday = HIGH_TEMPERATURES.today;
+  const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  
+  // Only change code above this line
