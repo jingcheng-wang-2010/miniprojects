1f70625a19212e785b33b19dae89213674a9bab0
Jingcheng Wang <jingwang49@gmail.com>
2023-11-11 00:48:36 +1100

    4 other methods

--- Basic Algorithm Scripting/basicAlgorithmScripting.js
+++ Basic Algorithm Scripting/basicAlgorithmScripting.js
@@ -141,3 +141,40 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// procedural approach
+
+function largestOfFour2(arr) {
+    const results = [];
+    for (let i = 0; i < arr.length; i++) {
+      let largestNumber = arr[i][0];
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > largestNumber) {
+          largestNumber = arr[i][j];
+        }
+      }
+      results[i] = largestNumber;
+    }
+  
+    return results;
+  }
+
+  // declarative approach
+  function largestOfFour3(arr) {
+    return arr.map(function(group) {
+      return group.reduce(function(prev, current) {
+        return current > prev ? current : prev;
+      });
+    });
+  }
+// declarative approach, map + apply.bind
+  function largestOfFour4(arr) {
+    return arr.map(Function.apply.bind(Math.max, null));
+  }
+
+// recursive approach
+function largestOfFour5(arr, finalArr = []) {
+    return !arr.length
+      ? finalArr
+      : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
+  }
