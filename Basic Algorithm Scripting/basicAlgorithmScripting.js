7ec540c276a873333863aea0d094b930c1d1178a
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 23:58:49 +1100

    Reverse a String

--- Basic Algorithm Scripting/basicAlgorithmScripting.js
+++ Basic Algorithm Scripting/basicAlgorithmScripting.js
@@ -4,6 +4,17 @@
 function convertCtoF(celsius) {
     let fahrenheit = celsius * 9 / 5 + 32;
     return fahrenheit;
-  }
+}
   
-  convertCtoF(30);
+convertCtoF(30);
+
+// Reverse a String
+function reverseString(str) {
+    let reversedStr = "";
+    for (let i = str.length-1; i > -1; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+  
+reverseString("hello"); 
