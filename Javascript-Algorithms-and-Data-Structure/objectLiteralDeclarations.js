40b2bf0775b2b3942b73db49d76ef73a6bc162bc
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 15:10:52 +1100

    Object Literal Declarations Using Object Shorthand

---
+++ Javascript-Algorithms-and-Data-Structure/objectLiteralDeclarations.js
@@ -0,0 +1,10 @@
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name: name,
+      age: age,
+      gender: gender
+    };
+    // Only change code above this line
+  };
