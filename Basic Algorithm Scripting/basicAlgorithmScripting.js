4f1003c773a26421aa3e3c0e57b557a9ef1ae410
Jingcheng Wang <jingwang49@gmail.com>
2023-11-11 00:43:59 +1100

    Return Largest Numbers in Arrays

--- Basic Algorithm Scripting/basicAlgorithmScripting.js
+++ Basic Algorithm Scripting/basicAlgorithmScripting.js
@@ -129,3 +129,15 @@ function factorialize3(num) {
           findLongestWordLength5(words.slice(1).join(" "))
         );
       }
+
+// Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+    let largestArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        largestArray.push(Math.max(...arr[i]));
+    }
+    return largestArray;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
