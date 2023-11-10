041f33e9396dc8040b50c32e2b61b978901c20a9
Jingcheng Wang <jingwang49@gmail.com>
2023-11-11 01:30:22 +1100

    2 other methods

--- Basic Algorithm Scripting/basicAlgorithmScripting.js
+++ Basic Algorithm Scripting/basicAlgorithmScripting.js
@@ -254,3 +254,17 @@ function findElement(arr, func) {
   }
   
   findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+  // find()
+  function findElement2(arr, func) {
+    return arr.find(func);
+  }
+
+  // recursive
+  function findElement3(arr, func) {
+    if (arr.length > 0 && !func(arr[0])) {
+      return findElement(arr.slice(1), func);
+    } else {
+      return arr[0];
+    }
+  }
