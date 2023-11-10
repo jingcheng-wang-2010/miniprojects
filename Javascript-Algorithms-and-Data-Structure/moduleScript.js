9eae882f7922b96cc78377739802f5439ff8c06b
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 15:40:37 +1100

    test complete

--- Javascript-Algorithms-and-Data-Structure/moduleScript.js
+++ Javascript-Algorithms-and-Data-Structure/moduleScript.js
@@ -2,7 +2,7 @@
 <html>
   <body>
     <!-- Only change code below this line -->
-
+    <script type="module" src="index.js"></script>
     <!-- Only change code above this line -->
   </body>
 </html>
