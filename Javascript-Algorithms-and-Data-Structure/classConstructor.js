00a756f32386a85b914bad14a6729f3aece4d7c2
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 15:28:04 +1100

    test complete

--- Javascript-Algorithms-and-Data-Structure/classConstructor.js
+++ Javascript-Algorithms-and-Data-Structure/classConstructor.js
@@ -1,6 +1,10 @@
 // Use class Syntax to Define a Constructor Function
 // Only change code below this line
-
+class Vegetable {
+    constructor(name) {
+        this.name = name;
+    }
+}
 // Only change code above this line
 
 const carrot = new Vegetable('carrot');
