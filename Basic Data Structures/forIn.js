9ca2ac806d822ec36683c19b0b0003176cd76c4e
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 23:24:34 +1100

    Iterate Through Keys of Object with for...in

---
+++ Basic Data Structures/forIn.js
@@ -0,0 +1,24 @@
+// Iterate Through the Keys of an Object with a for...in Statement
+const users = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
+  }
+  
+  function countOnline(allUsers) {
+    // Only change code below this line
+    let count = 0;
+    for (const user in allUsers) {
+        if (allUsers[user].online) { count++;}
+    }
+    return count;
+    // Only change code above this line
+  }
+  
+  console.log(countOnline(users));
