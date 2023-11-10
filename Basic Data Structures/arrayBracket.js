37ba3f8379f09130c794afa0b83f624f7ffbac08
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 21:27:20 +1100

    Access an Array's Contents Using Bracket Notation

---
+++ Basic Data Structures/arrayBracket.js
@@ -0,0 +1,6 @@
+// Access an Array's Contents Using Bracket Notation
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "bat";
+// Only change code above this line
+console.log(myArray);
