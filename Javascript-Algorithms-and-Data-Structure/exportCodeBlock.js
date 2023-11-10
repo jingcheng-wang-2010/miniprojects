8a5baded7bdb8352f093a1ccbec74a95b13c74a5
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 15:44:21 +1100

    Use export to Share a Code Block

---
+++ Javascript-Algorithms-and-Data-Structure/exportCodeBlock.js
@@ -0,0 +1,8 @@
+// Use export to Share a Code Block
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
