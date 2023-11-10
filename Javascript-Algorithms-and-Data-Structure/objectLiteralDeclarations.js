8b4cc21de9c185dd636c05648d8bdb3593e56d56
Jingcheng Wang <jingwang49@gmail.com>
2023-11-10 15:12:03 +1100

    test complete

--- Javascript-Algorithms-and-Data-Structure/objectLiteralDeclarations.js
+++ Javascript-Algorithms-and-Data-Structure/objectLiteralDeclarations.js
@@ -1,10 +1,4 @@
 // Write Concise Object Literal Declarations Using Object Property Shorthand
-const createPerson = (name, age, gender) => {
+const createPerson = (name, age, gender) => ({name, age, gender});
     // Only change code below this line
-    return {
-      name: name,
-      age: age,
-      gender: gender
-    };
     // Only change code above this line
-  };
