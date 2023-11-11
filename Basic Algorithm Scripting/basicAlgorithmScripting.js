4b4309533289326bda7425ff8edb03959aa31f20
Jingcheng Wang <jingwang49@gmail.com>
2023-11-11 22:03:02 +1100

    2 other methods

--- Basic Algorithm Scripting/basicAlgorithmScripting.js
+++ Basic Algorithm Scripting/basicAlgorithmScripting.js
@@ -319,3 +319,18 @@ function findElement(arr, func) {
   }
   
   frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+  // procedural, insert one by one
+  function frankenSplice2(arr1, arr2, n) {
+    // It's alive. It's alive!
+    let localArray = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+      localArray.splice(n, 0, arr1[i]);
+      n++;
+    }
+    return localArray;
+  }
+// slice() start end end, insert middle
+  function frankenSplice3(arr1, arr2, n) {
+    return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+  }
