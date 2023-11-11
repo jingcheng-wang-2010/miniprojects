80824d4e1b179bd879c6e9a10026bceede9918f0
Jingcheng Wang <jingwang49@gmail.com>
2023-11-11 21:58:55 +1100

    Slice and Splice

--- Basic Algorithm Scripting/basicAlgorithmScripting.js
+++ Basic Algorithm Scripting/basicAlgorithmScripting.js
@@ -307,3 +307,15 @@ function findElement(arr, func) {
       .toLowerCase()
       .replace(/(^|\s)\S/g, L => L.toUpperCase());
   }
+
+
+  //Slice and Splice
+  function frankenSplice(arr1, arr2, n) {
+    let arr3 = arr2.slice(0,arr2.length);
+    //console.log("1. arr3:", arr3);
+    arr3.splice(n, 0, ...arr1);
+    //console.log("2. arr3:", arr3);
+    return arr3;
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
