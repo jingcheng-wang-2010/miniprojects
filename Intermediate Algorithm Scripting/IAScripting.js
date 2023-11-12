a1cc7c0ab1c9e2d4e07d0e0f85c5d8121dd81311
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 16:12:48 +1100

    Pig Latin

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -263,4 +263,34 @@ function spinalCase2(str) {
   }
 
 
-  /* --- */
+/* --- */
+
+  /* Pig Latin
+Pig Latin is a way of altering English Words. The rules are as follows:
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+- If a word begins with a vowel, just add way at the end.
+*/
+
+function translatePigLatin(str) {
+    let newStr = "";
+    let vowelRegex = /[aeiou]/i;
+    // Check if the first character is a vowel
+    if (str[0].match(vowelRegex)){
+        newStr = str+"way";
+    } else if (str.match(vowelRegex) === null) {
+        // Check if the string contains only consonants
+        newStr = str + "ay";
+    } else {
+        // Find how many consonants before the first vowel
+        var vowelIndice = str.indexOf(str.match(vowelRegex)[0]);
+
+        // Take the string from the first vowel to the last char
+        // then add the consonants that were previously omitted and add the ending
+        newStr = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+    }
+    return newStr;
+  }
+  
+  translatePigLatin("consonant");
+
+/* --- */
