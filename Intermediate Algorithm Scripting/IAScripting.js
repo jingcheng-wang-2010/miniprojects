a63457127685d1eb73ee12867bf8c5dc042c66b8
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 16:29:57 +1100

    Search and Replace

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -333,3 +333,24 @@ function translatePigLatin4(str, charPos = 0) {
   }
 
 /* --- */
+
+/* Search and Replace
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+First argument is the sentence to perform the search and replace on.
+Second argument is the word that you will be replacing (before).
+Third argument is what you will be replacing the second argument with (after).
+*/
+
+function myReplace(str, before, after) {
+    let newAfter = after;
+    if (before[0] === before[0].toUpperCase()) {
+      newAfter = after[0].toUpperCase()+after.substring(1);
+    } else {
+      newAfter = after[0].toLowerCase()+after.substring(1);
+    }
+    return str.replace(before, newAfter);
+}
+  
+  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+/* --- */
