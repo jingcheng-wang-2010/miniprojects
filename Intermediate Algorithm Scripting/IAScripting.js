328b7e4b7b7fc7666bae5256940467bda880fa6e
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 17:25:10 +1100

    2 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -474,4 +474,29 @@ function fearNotLetter(str) {
   
 fearNotLetter("abce");
 
+// use charCodeAt()
+function fearNotLetter1(str) {
+    for (let i = 0; i < str.length; i++) {
+      /* code of current character */
+      const charCode = str.charCodeAt(i);
+  
+      /* if code of current character is not equal to first character + no of iteration
+          then a letter was skipped */
+      if (charCode !== str.charCodeAt(0) + i) {
+        /* if current character skipped past a character find previous character and return */
+        return String.fromCharCode(charCode - 1);
+      }
+    }
+    return undefined;
+  }
+
+  //shorter
+  function fearNotLetter2(str) {
+    for (let i = 1; i < str.length; ++i) {
+      if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+        return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+      }
+    }
+  }
+
 /* --- */
