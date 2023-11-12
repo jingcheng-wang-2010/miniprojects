9853da122fe74c7cfe14788f425d05c397a314fb
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 15:05:00 +1100

    4 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -75,5 +75,48 @@ function diffArray(arr1, arr2) {
   
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
+// Imperative Solution
+function diffArray2(arr1, arr2) {
+  const newArr = [];
 
+  function onlyInFirst(first, second) {
+    // Looping through an array to find elements that don't exist in another array
+    for (let i = 0; i < first.length; i++) {
+      if (second.indexOf(first[i]) === -1) {
+        // Pushing the elements unique to first to newArr
+        newArr.push(first[i]);
+      }
+    }
+  }
+
+  onlyInFirst(arr1, arr2);
+  onlyInFirst(arr2, arr1);
+
+  return newArr;
+}
+
+// Declarative Solution, concat then filter
+function diffArray3(arr1, arr2) {
+    return arr1
+        .concat(arr2)
+        .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+// Declarative Solution
+function diffArray4(arr1, arr2) {
+    return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+  
+    function diff(a, b) {
+      return a.filter(item => b.indexOf(item) === -1);
+    }
+}
+
+// Declarative Solution, using set
+function diffArray5(arr1, arr2) {
+    const difference = new Set(arr1);
+    arr2.forEach((ele) =>
+      difference.has(ele) ? difference.delete(ele) : difference.add(ele)
+    );
+    return Array.from(difference);
+  }
 /* --- */
