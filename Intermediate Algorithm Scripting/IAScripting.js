1be7ede324107ba04d83a01780fdd773dc6803bc
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 16:19:26 +1100

    4 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -293,4 +293,43 @@ function translatePigLatin(str) {
   
   translatePigLatin("consonant");
 
+
+// regex consonant
+function translatePigLatin1(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  translatePigLatin("consonant");
+
+// consonant cluster
+  function translatePigLatin2(str) {
+    if (str.match(/^[aeiou]/)) return str + "way";
+  
+    const consonantCluster = str.match(/^[^aeiou]+/)[0];
+    return str.substring(consonantCluster.length) + consonantCluster + "ay";
+  }
+
+// shorter
+function translatePigLatin3(str) {
+    return str
+      .replace(/^[aeiou]\w*/, "$&way")
+      .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+  }
+
+// shorter
+function translatePigLatin4(str, charPos = 0) {
+    return ['a', 'e', 'i', 'o', 'u'].includes(str[0])
+      ? str + (charPos === 0 ? 'way' : 'ay')
+      : charPos === str.length
+        ? str + 'ay'
+        : translatePigLatin(str.slice(1) + str[0], charPos + 1);
+  }
+
 /* --- */
