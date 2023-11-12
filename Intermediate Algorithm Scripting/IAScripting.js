dbd26d23bc11f33228c08f94a47bb29cb207aee1
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 14:59:53 +1100

    Diff Two Arrays

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -62,3 +62,18 @@ function sumAll4(arr) {
   }
 
 /* --- */
+
+/* Diff Two Arrays
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+Note: You can return the array with its elements in any order.
+*/
+
+function diffArray(arr1, arr2) {
+    const newArr = arr1.filter((val) => !arr2.includes(val)).concat(arr2.filter((val) => !arr1.includes(val)));
+    return newArr;
+}
+  
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+/* --- */
