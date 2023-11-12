cd76fb6271ee61abe58c88221b509351ecce319d
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 16:46:51 +1100

    DNA Pairing

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -404,3 +404,28 @@ function myReplace3(str, before, after) {
 
 
 /* --- */
+
+/* DNA Pairing
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
+function pairElement(str) {
+    const baseSet = {
+      A: ["A", "T"],
+      T: ["T", "A"],
+      C: ["C", "G"],
+      G: ["G", "C"]
+    }
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+      arr.push(baseSet[str[i]]);
+    }
+    return arr;
+}
+  
+pairElement("GCG");
+
+/* --- */
