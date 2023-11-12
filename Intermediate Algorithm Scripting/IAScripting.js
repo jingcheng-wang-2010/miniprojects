081a099400d20d5dee2bd813b539460597e418d0
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 16:33:57 +1100

    3 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -353,4 +353,54 @@ function myReplace(str, before, after) {
   
   myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
 
+// regex to check upper case
+function myReplace1(str, before, after) {
+    // Check if first character of argument "before" is a capital or lowercase letter and change the first character of argument "after" to match the case
+    if (/^[A-Z]/.test(before)) {
+      after = after[0].toUpperCase() + after.substring(1)
+    } else {
+      after = after[0].toLowerCase() + after.substring(1)
+    }
+  
+    // return string with argument "before" replaced by argument "after" (with correct case)
+    return str.replace(before, after);
+  }
+
+// if apply casing for every letter
+function myReplace2(str, before, after) {
+    // create a function that will change the casing of any number of letter in parameter "target"
+    // matching parameter "source"
+    function applyCasing(source, target) {
+      // split the source and target strings to array of letters
+      var targetArr = target.split("");
+      var sourceArr = source.split("");
+      // iterate through all the items of sourceArr and targetArr arrays till loop hits the end of shortest array
+      for (var i = 0; i < Math.min(targetArr.length, sourceArr.length); i++) {
+        // find out the casing of every letter from sourceArr using regular expression
+        // if sourceArr[i] is upper case then convert targetArr[i] to upper case
+        if (/[A-Z]/.test(sourceArr[i])) {
+          targetArr[i] = targetArr[i].toUpperCase();
+        }
+        // if sourceArr[i] is not upper case then convert targetArr[i] to lower case
+        else targetArr[i] = targetArr[i].toLowerCase();
+      }
+      // join modified targetArr to string and return
+      return targetArr.join("");
+    }
+  
+    // replace "before" with "after" with "before"-casing
+    return str.replace(before, applyCasing(before, after));
+  }
+
+// shorter
+function myReplace3(str, before, after) {
+    const strArr = str.split(" ");
+    const [wordToReplace] = strArr.filter(item => item === before);
+    const replacement = wordToReplace[0] === wordToReplace[0].toUpperCase()
+      ? after[0].toUpperCase() + after.slice(1)
+      : after[0].toLowerCase() + after.slice(1);
+    return strArr.map(item => (item === before ? replacement : item)).join(" ");
+  }
+
+
 /* --- */
