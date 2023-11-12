abbc1e201621fe83632a53836c04f30030239564
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 15:43:46 +1100

    Wherefore art thou

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -170,3 +170,18 @@ function destroyer3(arr, ...valsToRemove) {
 }
 
 /* --- */
+
+/* Wherefore art thou
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+*/
+
+function whatIsInAName(collection, source) {
+    const sourceKeys = Object.keys(source);
+    return collection
+        .filter(item => sourceKeys
+            .every(key => item[key] === source[key]));
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+/* --- */
