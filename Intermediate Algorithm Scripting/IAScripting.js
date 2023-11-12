fee1ffdc3e59c16ba2a5cd9c0d22916915f48c35
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 15:10:20 +1100

    Seek and Destroy

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -120,3 +120,20 @@ function diffArray5(arr1, arr2) {
     return Array.from(difference);
   }
 /* --- */
+
+/* Seek and Destroy
+You will be provided with an initial array as the first argument to the destroyer function, followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+The function must accept an indeterminate number of arguments, also known as a variadic function. You can access the additional arguments by adding a rest parameter to the function definition or using the arguments object.
+*/
+
+function destroyer(arr, ...args) {
+    let newArr = arr.slice();
+    for (let i = 0; i < args.length; i++){
+        newArr = newArr.filter((val) => !(val === args[i]));
+    }
+    return newArr;
+}
+  
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+/* --- */
