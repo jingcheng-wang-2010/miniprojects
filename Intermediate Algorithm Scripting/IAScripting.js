b28fa0329a6b8a748332dc0527c59f5a9303d9a7
Jingcheng Wang <jingwang49@gmail.com>
2023-11-12 15:56:00 +1100

    2 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -240,4 +240,27 @@ function spinalCase(str) {
   
 spinalCase('This Is Spinal Tap');
 
+// different order
+function spinalCase1(str) {
+    // Replace low-upper case to low-space-uppercase
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+    // Split on whitespace and underscores and join with dash
+    return str
+      .toLowerCase()
+      .split(/(?:_| )+/)
+      .join("-");
+  }
+  
+// shorter
+function spinalCase2(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+  
+    return str
+      .split(/\s|_|(?=[A-Z])/)
+      .join("-")
+      .toLowerCase();
+  }
+
+
   /* --- */
