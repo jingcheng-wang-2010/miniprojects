505f239c366ad59cd9ea5fa3b7ceb2a8c69f96e9
Jingcheng Wang <jingwang49@gmail.com>
2023-11-13 12:59:27 +1100

    Sum All Primes

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -701,3 +701,29 @@ function convertHTML2(str) {
     }
 
   /* --- */
+
+
+/* Sum All Primes
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+*/
+
+function sumPrimes(num) {
+  console.log("num:", num);
+  let primeSum = 0;
+  for (let i = 2; i <= num; i++){
+    let hasOtherDivisor = false;
+    for (let j = 2; j < i; j++){
+      if (i % j === 0) {
+        hasOtherDivisor = true;
+      }
+    }
+    if (!hasOtherDivisor) { primeSum += i };
+    console.log("hasOtherDivisor:", hasOtherDivisor, primeSum);
+  }
+  return primeSum;
+}
+
+sumPrimes(10);
+
+/* --- */
