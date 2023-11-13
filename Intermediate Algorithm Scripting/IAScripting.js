1a9dc330b208490561b9d14ba9043d92af9d1c1c
Jingcheng Wang <jingwang49@gmail.com>
2023-11-13 17:10:31 +1100

    Map the Debris

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -1198,3 +1198,34 @@ const Person = function(first, last) {
 
 /* --- */
 
+/* Map the Debris
+According to Kepler's Third Law, the orbital period  T
+  of two point masses orbiting each other in a circular or elliptic orbit is:
+
+T=2πa3μ−−−√
+ 
+a
+  is the orbit's semi-major axis
+μ=GM
+  is the standard gravitational parameter
+G
+  is the gravitational constant,
+M
+  is the mass of the more massive body.
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+*/
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  for (let i = 0; i < arr.length; i++) {
+    let T = Math.round(2 * Math.PI * (Math.sqrt(Math.pow(earthRadius + arr[i].avgAlt, 3) / GM)));
+    arr[i].orbitalPeriod = T;
+    delete arr[i].avgAlt;
+  }
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+/* --- */
