715f83cc706f2850f67c2ac0c29c0cad74f68af4
Jingcheng Wang <jingwang49@gmail.com>
2023-11-13 14:39:56 +1100

    3 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -1075,4 +1075,32 @@ function truthCheck(collection, pre) {
 
 truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
 
+// use hasOwnProperty to be sure property exists
+function truthCheck1(collection, pre) {
+  // Create a counter to check how many are true.
+  let counter = 0;
+  // Check for each object
+  for (let c in collection) {
+    // If it is has property and value is truthy
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+  // Outside the loop, check to see if we got true for all of them and return true or false
+  return counter == collection.length;
+}
+
+// shorter
+function truthCheck2(collection, pre) {
+  return collection.every(function (element) {
+    return element.hasOwnProperty(pre) && Boolean(element[pre]);
+  });
+}
+
+// even shorter
+function truthCheck3(collection, pre) {
+  // Is everyone being true?
+  return collection.every(obj => obj[pre]);
+}
+
 /* --- */
