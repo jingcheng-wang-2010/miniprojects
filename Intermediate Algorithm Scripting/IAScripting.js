da83bf8cc820e9d7521b822f03afee4ea143abbe
Jingcheng Wang <jingwang49@gmail.com>
2023-11-13 13:51:44 +1100

    3 other methods

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -795,4 +795,78 @@ function smallestCommons(arr) {
 
 smallestCommons([1,5]);
 
+
+// ES6 looping
+function smallestCommons1(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+  // Largest possible value for SCM
+  const upperBound = range.reduce((prod, curr) => prod * curr);
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    const divisible = range.every((value) => multiple % value === 0);
+    if (divisible) {
+      return multiple;
+    }
+  }
+}
+
+// finding GCD and LCM
+function smallestCommons2(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+  // GCD of two numbers
+  // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
+  const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
+  // LCM of two numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+  const lcm = (a, b) => a * b / gcd(a, b);
+  // LCM of multiple numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Other
+  return range.reduce((multiple, curr) => lcm(multiple, curr));
+}
+
+// Prime factorization
+// Find the SCM of a range of numbers
+function smallestCommons3(arr) {
+  const primeFactors = {};
+  const [min, max] = arr.sort((a, b) => a - b);
+  for (let i = min; i <= max; i++) {
+    // Factorize number in range
+    const currentFactors = getPrimeFactors(i);
+    for (let prime in currentFactors) {
+      // Add factor to set or update number of occurrences
+      if (!primeFactors[prime] || currentFactors[prime] > primeFactors[prime]) {
+        primeFactors[prime] = currentFactors[prime]
+      }
+    }
+  }
+  // Build SCM from factorization
+  let multiple = 1;
+  for (let prime in primeFactors) {
+    multiple *= prime ** primeFactors[prime];
+  }
+  return multiple;
+}
+
+// Compute prime factors of a number
+function getPrimeFactors(num) {
+  const factors = {};
+  for (let i = 2; i <= num; i++) {
+    // Count occurances of factor
+    // Note that composite values will not divide num
+    while ((num % i) === 0) {
+      factors[i] = (factors[i]) ? factors[i] + 1 : 1;
+      num /= i;
+    }
+  }
+  return factors;
+}
 /* --- */
