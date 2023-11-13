297634d455f75a069805fc61d9bd36b44e669606
Jingcheng Wang <jingwang49@gmail.com>
2023-11-13 16:44:43 +1100

    Make a Person

--- Intermediate Algorithm Scripting/IAScripting.js
+++ Intermediate Algorithm Scripting/IAScripting.js
@@ -1160,3 +1160,41 @@ function addTogether2() {
 }
 
 /* --- */
+
+/* Make a Person
+Fill in the object constructor with the following methods below:
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(first, last)
+Run the tests to see the expected output for each method. These methods must be the only available means of interacting with the object. Each test will declare a new Person instance as new Person('Bob', 'Ross').
+*/
+
+const Person = function(first, last) {
+  let firstName = first;
+  let lastName = last;
+  this.getFirstName = function() {
+    return firstName;
+  };
+  this.getLastName = function() {
+    return lastName;
+  };
+  this.getFullName = function() {
+    return this.getFirstName()+" "+this.getLastName();
+  };
+  this.setFirstName = function(first) {
+    firstName = first;
+  }
+  this.setLastName = function(last) {
+    lastName = last;
+  }
+  this.setFullName = function(first, last) {
+    this.setFirstName(first);
+    this.setLastName(last);
+  }
+};
+
+/* --- */
+
