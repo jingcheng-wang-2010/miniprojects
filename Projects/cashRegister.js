1d1a1aabfa45acff01d0d07a594339fad399fbb1
Jingcheng Wang <jingwang49@gmail.com>
2023-11-14 23:50:11 +1100

    working for many tests

--- Projects/cashRegister.js
+++ Projects/cashRegister.js
@@ -8,31 +8,59 @@ Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins 
 */
 
 function checkCashRegister(price, cash, cid) {
-    console.log("Initial:", price, cash, cid)
-    let currency = {
-        "PENNY": 0.01,
-        "NICKEL": 0.05,
-        "DIME": 0.1,
-        "QUARTER":0.25,
-        "DOLLAR": 1,
-        "FIVE": 5,
-        "TEN": 10,
-        "TWENTY": 20,
-        "ONE HUNDRED": 100
+    //console.log("Initial:", price, cash, cid)
+    let CURRENCY = {
+      "PENNY": 0.01,
+      "NICKEL": 0.05,
+      "DIME": 0.1,
+      "QUARTER":0.25,
+      "ONE": 1,
+      "FIVE": 5,
+      "TEN": 10,
+      "TWENTY": 20,
+      "ONE HUNDRED": 100
     };
+    let change = [];
     let changeRemain = cash - price;
     console.log("changeRemain:", changeRemain);
     const totalCID = Math.round(cid.reduce((sum, unit) => sum + unit[1], 0) * 100) / 100;
     console.log("totalCID:", totalCID);
     if (totalCID < changeRemain) { 
-        return {status: "INSUFFICIENT_FUNDS", change: []}
-    };
-    for (let i = cid.length-1; i < -1; i--) {
-        if (cid[i][1] < changeRemain) {
-          changeRemain -= cid[i][1];
+      return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+    if (totalCID === changeRemain) {
+      change = cid.filter((item) => item[1] > 0);
+      return {status: "CLOSED", change: change};
+    }
+    for (let i = cid.length-1; i > -1; i--) {
+      console.log("cid[i]:", cid[i]);
+      if (cid[i][1] < changeRemain) {
+        changeRemain -= cid[i][1];
+        changeRemain = Math.round(changeRemain * 100) / 100;
+        change.push(cid[i]);
+        console.log("LOOP1 changeRemain:", changeRemain);
+      } else {
+        let j = 0;
+        while (CURRENCY[cid[i][0]] * j <= changeRemain) {
+          j++;
         }
+        j--;
+        console.log("CURRENCY[cid[i][0]] * j:", CURRENCY[cid[i][0]] * j);
+        changeRemain -= CURRENCY[cid[i][0]] * j;
+        changeRemain = Math.round(changeRemain * 100) / 100;
+        change.push([cid[i][0], CURRENCY[cid[i][0]] * j]);
+        console.log("LOOP2 changeRemain:", changeRemain);
       }
-      return changeAmount;
+    }
+    console.log("changeRemain:", changeRemain);
+    console.log("change:", change);
+    if (changeRemain !== 0) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    } else {
+      change = change.filter((item) => item[1] > 0);
+      return {status: "OPEN", change: change};
+    }
+  
   }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
