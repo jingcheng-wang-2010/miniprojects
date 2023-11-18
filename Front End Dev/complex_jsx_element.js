c0792301c64dda5952049c6fa4228aacf38e0292
Jingcheng Wang <jingwang49@gmail.com>
2023-11-18 16:25:00 +1100

    Add Comments in JSX

--- Front End Dev/complex_jsx_element.js
+++ Front End Dev/complex_jsx_element.js
@@ -1,9 +1,12 @@
-const JSX = <div>
-  <h1>AAA</h1>
-  <p>bbb</p>
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ul>
-</div>;
+const JSX = (
+  <div>
+    {/* this is a comment */}
+    <h1>AAA</h1>
+    <p>bbb</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+)
