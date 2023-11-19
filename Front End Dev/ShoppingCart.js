c9fe731e8c47512794537c2b7ab8e29317634ebe
Jingcheng Wang <jingwang49@gmail.com>
2023-11-19 23:14:01 +1100

    Override Default Props

--- Front End Dev/ShoppingCart.js
+++ Front End Dev/ShoppingCart.js
@@ -1,12 +1,18 @@
-const ShoppingCart = (props) => {
-    return (
-    <div>
-      <h1>Shopping Cart Component</h1>
-    </div>
-  );
-
-};
-// Change code below this line
-ShoppingCart.defaultProps = {
-  items: 0
-};
+const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  }
+  
+  Items.defaultProps = {
+    quantity: 0
+  }
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      { /* Change code below this line */ }
+      return <Items quantity={10} />
+      { /* Change code above this line */ }
+    }
+  };
