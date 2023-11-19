9c5f2dba5f5514b8810f2cdefd370bd9f80692e9
Jingcheng Wang <jingwang49@gmail.com>
2023-11-19 23:17:29 +1100

    Use PropTypes to Define the Props You Expect

--- Front End Dev/ShoppingCart.js
+++ Front End Dev/ShoppingCart.js
@@ -1,7 +1,7 @@
 const Items = (props) => {
     return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
   }
-  
+  Items.propTypes = { quantity: PropTypes.number.isRequired};
   Items.defaultProps = {
     quantity: 0
   }
