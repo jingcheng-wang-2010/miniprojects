1e4bbc71df5bb8bb59dc01d03cdb54cf82ab44d7
Jingcheng Wang <jingwang49@gmail.com>
2023-11-19 23:29:56 +1100

    Review Using Props with Stateless Functional Components

---
+++ Front End Dev/CampSite.js
@@ -0,0 +1,25 @@
+class CampSite extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <Camper name='Jing'/>
+        </div>
+      );
+    }
+  };
+  // Change code below this line
+
+  const Camper = (props) => {
+    return (
+        <div>
+            <p>{props.name}</p>
+        </div>
+    );
+  }
+  Camper.propTypes= { name: PropTypes.string.isRequired }
+  Camper.defaultProps = {
+    name: 'CamperBot'
+  };
