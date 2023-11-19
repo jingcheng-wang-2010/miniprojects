37b3ebe19973e82468d2326025ddfd23e36bd8ac
Jingcheng Wang <jingwang49@gmail.com>
2023-11-19 23:37:35 +1100

    Render State in the User Interface Another Way

--- Front End Dev/MyComponent2.js
+++ Front End Dev/MyComponent2.js
@@ -6,12 +6,15 @@ class MyComponent2 extends React.Component {
       }
     }
     render() {
-      return (
-        <div>
-          { /* Change code below this line */ }
-          <h1>{this.state.name}</h1>
-          { /* Change code above this line */ }
-        </div>
-      );
+      // Change code below this line
+    const name = this.state.name;
+    // Change code above this line
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <h1>{name}</h1>
+        { /* Change code above this line */ }
+      </div>
+    );
     }
   };
