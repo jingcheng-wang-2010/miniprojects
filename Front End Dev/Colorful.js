081eaf41387d24c783da8a1da9286c97612b0305
Jingcheng Wang <jingwang49@gmail.com>
2023-11-21 23:55:55 +1100

    Add Inline Styles in React

--- Front End Dev/Colorful.js
+++ Front End Dev/Colorful.js
@@ -1,7 +1,15 @@
-class Colorful extends React.Component {
+const styles = {
+    color: "purple",
+    fontSize: 40,
+    border: "2px solid purple"
+  };
+  // Change code above this line
+  class Colorful extends React.Component {
     render() {
+      // Change code below this line
       return (
-        <div style={{color:"red", fontSize: 72}}>Big Red</div>
+        <div style={styles}>Style Me!</div>
       );
+      // Change code above this line
     }
   };
