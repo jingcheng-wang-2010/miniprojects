1dd79ff042f871da590f6362cfe92ac788f82fad
Jingcheng Wang <jingwang49@gmail.com>
2023-11-27 23:30:39 +1100

    handleChange

--- Front End Dev/MyComponent8.js
+++ Front End Dev/MyComponent8.js
@@ -1,24 +1,37 @@
-class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        display: true
-      }
-      this.toggleDisplay = this.toggleDisplay.bind(this);
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
     }
-    toggleDisplay() {
-      this.setState((state) => ({
-        display: !state.display
-      }));
-    }
-    render() {
-      // Change code below this line
-        return (
-          <div>
-            <button onClick={this.toggleDisplay}>Toggle Display</button>
-            {this.state.display && <h1>Displayed!</h1>}
-          </div>
-        );
-      }
-    }
-  };
+    //this.handleChange = this.handleChange.bind(this);
+    //this.submitMessage = this.submitMessage.bind(this);
+  }
+  // Add handleChange() and submitMessage() methods here
+  handleChange(e){
+    this.setState({
+      input: e.target.value,
+      messages: this.state.messages
+    });
+  }
+  submitMessage(e) {
+    this.setState({
+      input: '',
+      messages: [...this.state.messages, this.state.input]
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* Render an input, button, and ul below this line */ }
+        <div>{this.state.input}</div>
+        <input onChange={this.handleChange.bind(this)} value={this.state.input} />
+        <button onClick={this.submitMessage.bind(this)}>Submit</button>
+        <ul></ul>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
