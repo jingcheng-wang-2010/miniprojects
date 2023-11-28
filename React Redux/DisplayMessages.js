82e70f81b0dba79672fd0bc22d5c75eb9d5965f2
Jingcheng Wang <jingwang49@gmail.com>
2023-11-28 23:28:31 +1100

    Getting Started with React Redux

--- React Redux/DisplayMessages.js
+++ React Redux/DisplayMessages.js
@@ -7,15 +7,33 @@ class DisplayMessages extends React.Component {
         messages: []
       }
     };
-    // Add handleChange() and submitMessage() methods here
-    render() {
-      return (
-        <div>
-            <h2>Type in a new Message:</h2>
-            { /* Render an input, button, and ul below this line */ }
-
-            { /* Change code above this line */ }
-        </div>
-      )
-    }
-  };
+    //this.handleChange = this.handleChange.bind(this);
+    //this.submitMessage = this.submitMessage.bind(this);
+  }
+  // Add handleChange() and submitMessage() methods here
+  handleChange(e){
+    this.setState({
+      input: e.target.value,
+      messages: this.state.messages
+    });
+  }
+  submitMessage(e) {
+    this.setState({
+      input: '',
+      messages: [...this.state.messages, this.state.input]
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* Render an input, button, and ul below this line */ }
+        <div>{this.state.input}</div>
+        <input onChange={this.handleChange.bind(this)} value={this.state.input} />
+        <button onClick={this.submitMessage.bind(this)}>Submit</button>
+        <ul></ul>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
