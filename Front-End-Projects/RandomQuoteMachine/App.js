c3875eb8f0659d46e85ba7236a670f4096797ec6
Jingcheng Wang <jingwang49@gmail.com>
2023-11-30 22:37:40 +1100

    setup random quote machine

---
+++ Front-End-Projects/RandomQuoteMachine/App.js
@@ -0,0 +1,30 @@
+import React from "https://esm.sh/react"
+import ReactDOM from "https://esm.sh/react-dom"
+//import { Provider, connect } from "https://esm.sh/react-redux"
+//import { createStore, combineReducers, applyMiddleware } from "https://esm.sh/redux"
+//import thunk from "https://esm.sh/redux-thunk"
+
+//import rootReducer from './redux/reducers'
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <div id="quote-box">
+                    <p id="text">text</p>
+                    <p id="author">author</p>
+                    <button id="new-quote">New Quote</button>
+                    <a id="tweet-quote">tweet this</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
