b6fdf49a31c2f1431f2cd8d81017e9d055916e9a
Jingcheng Wang <jingwang49@gmail.com>
2023-12-07 12:16:56 +1100

    use marked parse and dangerouslySetInnerHTML

--- Projects/markdown-previewer/src/App.js
+++ Projects/markdown-previewer/src/App.js
@@ -1,14 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'https://cdnjs.cloudflare.com/ajax/libs/marked/11.0.0/marked.min.js'
+import { marked } from 'marked'
+//import 'https://cdnjs.cloudflare.com/ajax/libs/marked/11.0.0/marked.min.js'
 import './App.css'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      content: '### Enter Markdown Here'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState(() => ({
+      content: e.target.value
+    }))
+  }
+
+  rawMarkup () {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false
+      //highlight: function (code) {
+      //  return hljs.highlightAuto(code).value
+      //}
+    })
+
+    var rawMarkup = marked(this.state.content, { sanitize: true })
+    return {
+      __html: rawMarkup
+    }
   }
 
   render () {
+    console.log(this.rawMarkup)
     return (
       <div className='App'>
         <div className='editorWrap'>
@@ -17,9 +50,12 @@ class App extends React.Component {
             Editor
             <i className='fa fa-arrows-alt'></i>
           </div>
-          <textarea id='editor' type='text'>
-            Welcome
-          </textarea>
+          <textarea
+            id='editor'
+            type='text'
+            defaultValue={this.state.content}
+            onChange={this.handleChange}
+          ></textarea>
         </div>
         <div className='previewWrap'>
           <div className='toolbar'>
@@ -27,10 +63,7 @@ class App extends React.Component {
             Previewer
             <i className='fa fa-arrows-alt'></i>
           </div>
-          <div id='preview'>
-            Welcome
-          </div>
-          
+          <div id='preview' dangerouslySetInnerHTML={this.rawMarkup()}></div>
         </div>
       </div>
     )
