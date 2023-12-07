1621a77cc71dab6df6b8a9661976d0946f221fc1
Jingcheng Wang <jingwang49@gmail.com>
2023-12-07 11:37:56 +1100

    basic look setup

--- Projects/markdown-previewer/src/App.js
+++ Projects/markdown-previewer/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'https://cdnjs.cloudflare.com/ajax/libs/marked/11.0.0/marked.min.js'
 import './App.css'
 
 class App extends React.Component {
@@ -8,7 +9,31 @@ class App extends React.Component {
   }
 
   render () {
-    return <div className='App'></div>
+    return (
+      <div className='App'>
+        <div className='editorWrap'>
+          <div className='toolbar'>
+            <i className='fa fa-free-code-camp'></i>
+            Editor
+            <i className='fa fa-arrows-alt'></i>
+          </div>
+          <textarea id='editor' type='text'>
+            Welcome
+          </textarea>
+        </div>
+        <div className='previewWrap'>
+          <div className='toolbar'>
+            <i className='fa fa-free-code-camp'></i>
+            Previewer
+            <i className='fa fa-arrows-alt'></i>
+          </div>
+          <div id='preview'>
+            Welcome
+          </div>
+          
+        </div>
+      </div>
+    )
   }
 }
 
