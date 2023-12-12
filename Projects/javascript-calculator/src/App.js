c43f54da241a8e0caa3f8ebb051b3dc2f8e8a6ae
Jingcheng Wang <jingwang49@gmail.com>
2023-12-12 16:03:15 +1100

    basic css look

--- Projects/javascript-calculator/src/App.js
+++ Projects/javascript-calculator/src/App.js
@@ -4,24 +4,24 @@ function App() {
   return (
     <div className="App">
       <div className="calculator-container">
-        <div id="display"></div>
-        <div id="clear">AC</div>
-        <div id="divide">/</div>
-        <div id="multiply">X</div>
-        <div id="seven">7</div>
-        <div id="eight">8</div>
-        <div id="nine">9</div>
-        <div id="subtract">-</div>
-        <div id="four">4</div>
-        <div id="five">5</div>
-        <div id="six">6</div>
-        <div id="add">+</div>
-        <div id="one">1</div>
-        <div id="two">2</div>
-        <div id="three">3</div>
-        <div id="equal">=</div>
-        <div id="three">3</div>
-        <div id="decimal">.</div>
+        <div id="display">0</div>
+        <button id="clear" className="horizontal-3">AC</button>
+        <button id="divide" className="operator">/</button>
+        <button id="seven">7</button>
+        <button id="eight">8</button>
+        <button id="nine">9</button>
+        <button id="multiply" className="operator">X</button>
+        <button id="four">4</button>
+        <button id="five">5</button>
+        <button id="six">6</button>
+        <button id="subtract" className="operator">-</button>
+        <button id="one">1</button>
+        <button id="two">2</button>
+        <button id="three">3</button>
+        <button id="add" className="operator">+</button>
+        <button id="zero" className="horizontal-2">0</button>
+        <button id="decimal">.</button>
+        <button id="equal" className="operator">=</button>
       </div>
     </div>
   );
