47463801d665fc1ad656015d87eae9bf5a75b60b
Jingcheng Wang <jingwang49@gmail.com>
2023-12-12 15:38:14 +1100

    all displayed elements

--- Projects/javascript-calculator/src/App.js
+++ Projects/javascript-calculator/src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="calculator-container">
+        <div id="display"></div>
+        <div id="clear">AC</div>
+        <div id="divide">/</div>
+        <div id="multiply">X</div>
+        <div id="seven">7</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
+        <div id="subtract">-</div>
+        <div id="four">4</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
+        <div id="add">+</div>
+        <div id="one">1</div>
+        <div id="two">2</div>
+        <div id="three">3</div>
+        <div id="equal">=</div>
+        <div id="three">3</div>
+        <div id="decimal">.</div>
+      </div>
     </div>
   );
 }
