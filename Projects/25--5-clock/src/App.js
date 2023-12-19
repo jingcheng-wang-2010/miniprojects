10360e0c3ca8539d1529ccad2f4f93082166403f
Jingcheng Wang <jingwang49@gmail.com>
2023-12-19 20:09:28 +1100

    html and css done

--- Projects/25--5-clock/src/App.js
+++ Projects/25--5-clock/src/App.js
@@ -1,39 +1,47 @@
-import './App.css';
+import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp, faArrowDown, faPlay, faPause, faArrowsRotate } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowUp,
+  faArrowDown,
+  faPlay,
+  faPause,
+  faArrowsRotate
+} from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <p>25 + 5 Clock</p>
+    <div className='app-container'>
       <div>
-        <p id="break-label">Break Length</p>
-        <p>
-          <FontAwesomeIcon icon={faArrowUp} id="break-increment" />
-          <span id="break-length">5</span>
-          <FontAwesomeIcon icon={faArrowDown} id="break-decrement" />
-        </p>
+        <p className='app-title'>25 + 5 Clock</p>
+        <div className='length-container'>
+          <p id='break-label'>Break Length</p>
+          <p>
+            <FontAwesomeIcon icon={faArrowUp} id='break-increment' />
+            <span id='break-length'>5</span>
+            <FontAwesomeIcon icon={faArrowDown} id='break-decrement' />
+          </p>
+        </div>
+        <div className='length-container'>
+          <p id='session-label'>Session Length</p>
+          <p>
+            <FontAwesomeIcon icon={faArrowUp} id='session-increment' />
+            <span id='session-length'>25</span>
+            <FontAwesomeIcon icon={faArrowDown} id='session-decrement' />
+          </p>
+        </div>
+        <div>
+          <p id='timer-label'>Session</p>
+          <p id='time-left'>25:00</p>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faPlay} id='start_stop' />
+          <FontAwesomeIcon icon={faPause} />
+          <FontAwesomeIcon icon={faArrowsRotate} id='reset' />
+        </div>
+        <audio id='beep' />
       </div>
-      <div>
-        <p id="session-label">Session Length</p>
-        <p>
-          <FontAwesomeIcon icon={faArrowUp} id="session-increment" />
-          <span id="session-length">25</span>
-          <FontAwesomeIcon icon={faArrowDown} id="session-decrement"/>
-        </p>
-      </div>
-      <div>
-        <p id="timer-label">Session</p>
-        <p id="time-left">25:00</p>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faPlay} id="start_stop" />
-        <FontAwesomeIcon icon={faPause} />
-        <FontAwesomeIcon icon={faArrowsRotate} id="reset" />
-      </div>
-      <audio id="beep" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
