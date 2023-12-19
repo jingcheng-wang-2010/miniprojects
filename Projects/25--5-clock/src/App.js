1382fb2321197aaaf03609157f2cfe8d309cc08d
Jingcheng Wang <jingwang49@gmail.com>
2023-12-19 21:15:50 +1100

    update session and break values, triggering timer

--- Projects/25--5-clock/src/App.js
+++ Projects/25--5-clock/src/App.js
@@ -7,8 +7,28 @@ import {
   faPause,
   faArrowsRotate
 } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react'
 
 function App () {
+  const [breakValue, setBreakValue] = useState(5);
+  const [sessionValue, setSessionValue] = useState(25);
+  const [timer, setTimer] = useState("25:00");
+
+  const updateBreak = n => {
+    const newValue = breakValue + n;
+    if (newValue > 0 && newValue <= 60) {
+      setBreakValue(newValue);
+    }
+  }
+
+  const updateSession= n => {
+    const newValue = sessionValue + n;
+    if (newValue > 0 && newValue <= 60) {
+      setSessionValue(newValue);
+      setTimer(newValue+":00");
+    }
+  }
+
   return (
     <div className='app-container'>
       <div>
@@ -16,22 +36,22 @@ function App () {
         <div className='length-container'>
           <p id='break-label'>Break Length</p>
           <p>
-            <FontAwesomeIcon icon={faArrowUp} id='break-increment' />
-            <span id='break-length'>5</span>
-            <FontAwesomeIcon icon={faArrowDown} id='break-decrement' />
+            <FontAwesomeIcon icon={faArrowUp} id='break-increment' onClick={() => updateBreak(1)} />
+            <span id='break-length'>{breakValue}</span>
+            <FontAwesomeIcon icon={faArrowDown} id='break-decrement' onClick={() => updateBreak(-1)} />
           </p>
         </div>
         <div className='length-container'>
           <p id='session-label'>Session Length</p>
           <p>
-            <FontAwesomeIcon icon={faArrowUp} id='session-increment' />
-            <span id='session-length'>25</span>
-            <FontAwesomeIcon icon={faArrowDown} id='session-decrement' />
+            <FontAwesomeIcon icon={faArrowUp} id='session-increment' onClick={() => updateSession(1)} />
+            <span id='session-length'>{sessionValue}</span>
+            <FontAwesomeIcon icon={faArrowDown} id='session-decrement' onClick={() => updateSession(-1)} />
           </p>
         </div>
         <div>
           <p id='timer-label'>Session</p>
-          <p id='time-left'>25:00</p>
+          <p id='time-left'>{timer}</p>
         </div>
         <div>
           <FontAwesomeIcon icon={faPlay} id='start_stop' />
