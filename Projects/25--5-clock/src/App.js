8319b4134887db024ca02223d7a81ef6a7508fe9
Jingcheng Wang <jingwang49@gmail.com>
2023-12-19 19:27:33 +1100

    install font awesome

--- Projects/25--5-clock/src/App.js
+++ Projects/25--5-clock/src/App.js
@@ -3,7 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <p>25 + 5 Clock</p>
+      <div>
+        <p id="break-label">Break Length</p>
+        <p>
+          <i className='fa fa-arrow-up'></i>
+          5
+          <i className='fa fa-arrow-down'></i>
+        </p>
+      </div>
+      <div>
+        <p>Session Length</p>
+        <p>
+          <i className='fa fa-arrow-up'></i>
+          25
+          <i className='fa fa-arrow-down'></i>
+        </p>
+      </div>
+      <div>
+        <p>Session</p>
+        <p>25:00</p>
+      </div>
+      <div>
+        <i className='fa fa-play'></i>
+        <i className='fa fa-pause'></i>
+        <i className='fa fa-arrows-rotate'></i>
+      </div>
     </div>
   );
 }
