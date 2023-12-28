a99068535d0d42ffc2ae466a7f29b4a086d9adfb
Jingcheng Wang <jingwang49@gmail.com>
2023-12-28 14:49:52 +1100

    change text for button 1 onclick

--- Projects/javascript-algorithms-data-structures/RPG/script.js
+++ Projects/javascript-algorithms-data-structures/RPG/script.js
@@ -23,6 +23,7 @@ button2.onclick = goCave
 button3.onclick = fightDragon
 
 function goStore () {
+    text.innerText = "You enter the store."
   button1.innerText = 'Buy 10 health (10 gold)'
   button2.innerText = 'Buy weapon (30 gold)'
   button3.innerText = 'Go to town square'
